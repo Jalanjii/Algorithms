c5b7a1316828963daef33e7476418276774585a2
Jalanji <30749788+Jalanjii@users.noreply.github.com>
2021-04-04 21:41:29 +0300

    Update 139.cpp

--- 139.cpp
+++ 139.cpp
@@ -20,35 +20,33 @@ int mlt(int s[], int p[], int i, int j) {
 
 int partition(int A[], int s[], int p[], int start, int end) {
 	int i = start + 1;
-	int piv = A[start];            //make the first element as pivot element.
+	int piv = A[start];           
 	for (int j = start + 1; j <= end; j++) {
-		/*rearrange the array by putting elements which are less than pivot
-		on one side and which are greater that on other. */
+		
 
 		if (mlt(s, p, A[j], piv)) {
 			swap(A[i], A[j]);
 			i += 1;
 		}
 	}
-	swap(A[start], A[i - 1]);  //put the pivot element in its proper place.
-	return i - 1;                      //return the position of the pivot
+	swap(A[start], A[i - 1]);  
+	return i - 1;                     
 }
 
 int rand_partition(int A[], int s[], int p[], int start, int end) {
-	//chooses position of pivot randomly by using rand() function .
+	
 	int random = start + rand() % (end - start + 1);
 
-	swap(A[random], A[start]);        //swap pivot with 1st element.
-	return partition(A, s, p, start, end);       //call the above partition function
+	swap(A[random], A[start]);        
+	return partition(A, s, p, start, end);       
 }
 
 void quickSort(int A[], int s[], int p[], int start, int end) {
 
 	if (start < end) {
-		//stores the position of pivot element
 		int piv_pos = rand_partition(A, s, p, start, end);
-		quickSort(A, s, p, start, piv_pos - 1);    //sorts the left side of pivot.
-		quickSort(A, s, p, piv_pos + 1, end); //sorts the right side of pivot.
+		quickSort(A, s, p, start, piv_pos - 1);    
+		quickSort(A, s, p, piv_pos + 1, end); 
 	}
 }
 
